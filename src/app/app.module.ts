07fdf3ea7d5637f2b36b9670d4a0c19e280c7edc
Евгения <evg_k@inpris.ru>
2021-03-29 11:18:35 +0300

    проект-библиотека с less

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimaryButtonComponent } from './button/primary-button/primary-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryButtonComponent
   ],
   imports: [
     BrowserModule
