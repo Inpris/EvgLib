9eb24104db458e23035d01d5a7559b5d9f2b517e
Евгения <evg_k@inpris.ru>
2021-03-29 16:52:51 +0300

    сделала кнопку и могу менять её размер

--- src/app/button/primary-button/primary-button.component.ts
+++ src/app/button/primary-button/primary-button.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./primary-button.component.less']
 })
 export class PrimaryButtonComponent implements OnInit {
+  public className: string = 'primary-button__large';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public set_className(name: number): void {
+    if (name === 1){
+      this.className = 'primary-button__small';
+    }
+    if (name === 2){
+      this.className = 'primary-button__default';
+    }
+    if (name === 3){
+      this.className = 'primary-button__large';
+    }
+  }
 }
