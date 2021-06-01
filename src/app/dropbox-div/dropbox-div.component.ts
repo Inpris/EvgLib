54749eda58d9bad77b9e0cbd947dfc92a4a3c19f
Inpris <evg_k@inpris.ru>
2021-06-01 16:54:46 +0300

    Закончила дропдаун с помощью дивов. Стили собственные, всё работает, проблем нет.

--- src/app/dropbox-div/dropbox-div.component.ts
+++ src/app/dropbox-div/dropbox-div.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ControlValueAccessor, FormBuilder} from '@angular/forms';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-dropbox-div',
@@ -7,41 +9,35 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./dropbox-div.component.less']
 })
 export class DropboxDivComponent implements OnInit {
+  @ViewChild('dropdown', { read: ElementRef }) public dropdown: ElementRef;
+
   @Input() public disabled: boolean = false;
   @Input() public list = [];
   @Input() public form;
   @Input() public firstText = '';
+  @Input() value: string;
   isSubmitted: boolean = false;
+
   // dropboxValue: string = this.firstText;
 
   constructor(private _fb: FormBuilder) { }
 
-  ngOnInit(): void {
-    // this.list.splice(0, 0, this.firstText);
-  }
-
-  // changeRole(event) {
-  //   console.log(event.value);
-  //
-  //   this.divRoleName.setValue(event.target.value, {
-  //     onlySelf: true
-  //   });
-  // }
+  ngOnInit(): void {}
 
   show() {
     this.isSubmitted = !this.isSubmitted;
   }
 
-  setValue(itemName) {
+  setValue($event, itemName) {
     this.firstText = itemName;
+    this.isSubmitted = !this.isSubmitted;
 
-    this.divRoleName.setValue(itemName, {
-      onlySelf: true
-    });
+    this.divRoleName.setValue(itemName);
   }
 
 //  геттер для roleForm
   get divRoleName() {
     return this.form.get('divRoleName');
   }
+
 }
